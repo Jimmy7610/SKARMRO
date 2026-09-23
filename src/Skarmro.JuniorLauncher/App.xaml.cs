5c1346f506b2d6b5036ab0503acf0223d631b867
Jimmy <164167870+Jimmy7610@users.noreply.github.com>
2026-09-23 09:22:16 +0200

    Enforce single Junior Launcher instance

--- src/Skarmro.JuniorLauncher/App.xaml.cs
+++ src/Skarmro.JuniorLauncher/App.xaml.cs
@@ -1,7 +1,42 @@
+using System.Threading;
 using System.Windows;
 
 namespace Skarmro.JuniorLauncher;
 
 public partial class App : Application
 {
+    private Mutex? _singleInstanceMutex;
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        _singleInstanceMutex = new Mutex(
+            initiallyOwned: true,
+            name: @"Local\SKARMRO-JuniorLauncher",
+            createdNew: out var createdNew);
+
+        if (!createdNew)
+        {
+            Shutdown(0);
+            return;
+        }
+
+        var window = new MainWindow();
+        MainWindow = window;
+        window.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            _singleInstanceMutex?.ReleaseMutex();
+        }
+        catch
+        {
+            // The mutex may already have been released during abnormal shutdown.
+        }
+
+        _singleInstanceMutex?.Dispose();
+        base.OnExit(e);
+    }
 }
