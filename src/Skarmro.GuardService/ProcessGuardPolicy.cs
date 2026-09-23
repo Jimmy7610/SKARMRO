51d0a64f3a3f7d186a1a8f38500988616cfa73f3
Jimmy <164167870+Jimmy7610@users.noreply.github.com>
2026-09-23 08:48:33 +0200

    Version native app policy contract

--- src/Skarmro.GuardService/ProcessGuardPolicy.cs
+++ src/Skarmro.GuardService/ProcessGuardPolicy.cs
@@ -2,8 +2,11 @@ namespace Skarmro.GuardService;
 
 public sealed class ProcessGuardPolicy
 {
+    public int PolicyVersion { get; set; } = 1;
+    public string UpdatedAtUtc { get; set; } = "";
     public bool Enabled { get; set; }
     public string ChildSid { get; set; } = "";
     public string[] BlockedProcessNames { get; set; } = Array.Empty<string>();
     public string[] BlockedSha256 { get; set; } = Array.Empty<string>();
+    public string[] AllowedProcessNames { get; set; } = Array.Empty<string>();
 }
